30837711dcd222e58d7e5226c0acf905e464866b
Olivier Mourlevat <olivier.mourlevat@gmail.com>
2014-05-27 18:37:52 +0200

    add answers test

---
+++ app/data/answers.js
@@ -0,0 +1,21 @@
+{
+	"answers": {
+		"hugo-lloris": {
+			"famille": "d",
+			"chef": "d",
+			"film": "a",
+			"enfance": "d",
+			"autobio": "d",
+			"vetements": "b",
+			"plat": "a",
+			"surnom": "b",
+			"detail": "c",
+			"argent": "c",
+			"reve": "b",
+			"tempslibre": "e"
+		},
+		"karim-valbuena": {
+
+		}
+	}
+}
--- sql/init.sql
+++ sql/init.sql
@@ -1,9 +1,7 @@
-CREATE TABLE IF NOT EXISTS `optin_email` (
+CREATE TABLE `optin_email` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `created_at` datetime NOT NULL,
-  `email` varchar(255) DEFAULT NULL,
+  `email` varchar(255) NOT NULL DEFAULT '',
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
 ) DEFAULT CHARSET=utf8;
-
-
